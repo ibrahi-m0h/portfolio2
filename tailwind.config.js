fde1287c06994f6a1b5cd340a8ee3b4f4ca48d61
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2023-06-02 14:41:24 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   'merriweather': ['Merriweather', 'serif']
+      // }
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
